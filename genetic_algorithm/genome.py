16b55825bafe5e6f9122d9807ab414e7bdbaf47f
Michał Zawadzki <michauzawadzki@gmail.com>
2021-05-02 22:10:07 +0200

    Rewrite Genome class: add Callable parameter 'rank'

--- genetic_algorithm/genome.py
+++ genetic_algorithm/genome.py
@@ -1,16 +1,15 @@
+from typing import Callable
+
 import numpy as np
 
 
 class Genome:
     dimension: int
+    rank: Callable[[np.ndarray], float]
 
-    def __init__(self, dimension: int):
+    def __init__(self, dimension: int, rank: Callable[[np.ndarray], float]):
         self.dimension = dimension
+        self.rank = rank
 
     def rank(self, array: np.ndarray) -> float:
-        pass
-
-
-class QuadraticFunctionGenome(Genome):
-    def rank(self, array: np.ndarray) -> float:
-        return float(np.sum(array ** 2))
+        return self.rank(array)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from typing import Callable
+
+import numpy as np
+
 from genetic_algorithm.crossover import OnePointCrossover
 from genetic_algorithm.genetic_algorithm import GeneticAlgorithm
-from genetic_algorithm.genome import QuadraticFunctionGenome
+from genetic_algorithm.genome import Genome
 from genetic_algorithm.mutation import RandomMutation
 from genetic_algorithm.parent_selection import TournamentSelection
 
@@ -17,7 +21,9 @@ class GeneticAlgorithmImpl(GeneticAlgorithm):
         return RandomMutation().mutate(crossovers)
 
 
-genome = QuadraticFunctionGenome(10)
+quadratic_rank: Callable[[np.ndarray], float] = lambda array: float(np.sum(array ** 2))
+
+genome = Genome(10, quadratic_rank)
 algorithm_impl = GeneticAlgorithmImpl(genome=genome, population_size=20, gene_low=-10.0, gene_high=10.0,
                                       best_last_generations_size=100)
 algorithm_impl.calculate()
