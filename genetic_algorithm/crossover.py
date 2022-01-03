c9be2490ba7de95b84cc2a7cf26b4db02e854799
Michał Zawadzki <michauzawadzki@gmail.com>
2022-01-03 20:26:34 +0100

    Genetic algorithm: implemented TwoPointsCrossover

--- genetic_algorithm/crossover.py
+++ genetic_algorithm/crossover.py
@@ -1,5 +1,6 @@
-import copy
 from typing import Sequence
+
+import numpy as np
 
 from genetic_algorithm.genome import Genome
 from utils.math import MathUtil
@@ -14,18 +15,45 @@ def select_crossover_point(size: int) -> int:
     return MathUtil.random_int_from_range(1, size - 2)
 
 
+def select_two_crossover_points(size: int) -> [int]:
+    points = np.random.choice(a=range(1, size - 2), replace=False, size=2).tolist()
+    points.sort()
+    return points
+
+
 class OnePointCrossover(Crossover):
     def crossover(self, parents: Sequence[Genome]) -> Sequence[Genome]:
-        crossover1 = copy.deepcopy(parents[0])
-        crossover2 = copy.deepcopy(parents[1])
-        genes1 = crossover1.genes
-        genes2 = crossover2.genes
+        genes1 = parents[0].genes
+        genes2 = parents[1].genes
         size = len(genes1)
         crossover_point = select_crossover_point(size)
         genes1_1 = genes1.select_range(0, crossover_point)
         genes1_2 = genes1.select_range(crossover_point, size)
         genes2_1 = genes2.select_range(0, crossover_point)
         genes2_2 = genes2.select_range(crossover_point, size)
-        crossover1.genes = genes1_1 + genes2_2
-        crossover2.genes = genes2_1 + genes1_2
+        rank_func = parents[0].rank_func
+        crossover1 = Genome(genes=genes1_1 + genes2_2, rank_funk=rank_func)
+        crossover2 = Genome(genes=genes2_1 + genes1_2, rank_funk=rank_func)
         return [crossover1, crossover2]
+
+
+class TwoPointsCrossover(Crossover):
+    def crossover(self, parents: Sequence[Genome]) -> Sequence[Genome]:
+        genes1 = parents[0].genes
+        genes2 = parents[1].genes
+        size = len(genes1)
+        crossover_point1, crossover_point2 = select_two_crossover_points(size)
+        genes1_1 = genes1.select_range(0, crossover_point1)
+        genes1_2 = genes1.select_range(crossover_point1, crossover_point2)
+        genes1_3 = genes1.select_range(crossover_point2, size)
+        genes2_1 = genes2.select_range(0, crossover_point1)
+        genes2_2 = genes2.select_range(crossover_point1, crossover_point2)
+        genes2_3 = genes2.select_range(crossover_point2, size)
+        rank_func = parents[0].rank_func
+        crossover1 = Genome(genes=genes2_1 + genes1_2 + genes2_3, rank_funk=rank_func)
+        crossover2 = Genome(genes=genes1_1 + genes2_2 + genes1_3, rank_funk=rank_func)
+        crossover3 = Genome(genes=genes1_1 + genes2_2 + genes2_3, rank_funk=rank_func)
+        crossover4 = Genome(genes=genes1_1 + genes1_2 + genes2_3, rank_funk=rank_func)
+        crossover5 = Genome(genes=genes2_1 + genes1_2 + genes1_3, rank_funk=rank_func)
+        crossover6 = Genome(genes=genes2_1 + genes2_2 + genes1_3, rank_funk=rank_func)
+        return [crossover1, crossover2, crossover3, crossover4, crossover5, crossover6]
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from typing import Sequence, MutableSequence
 
 import numpy as np
 
-from genetic_algorithm.crossover import OnePointCrossover
+from genetic_algorithm.crossover import TwoPointsCrossover
 from genetic_algorithm.gene import Gene, FloatGene, IntegerGene
 from genetic_algorithm.genetic_algorithm import GeneticAlgorithm
 from genetic_algorithm.genome import Genome, LabeledSequence
 from genetic_algorithm.initialization import RandomInitialization
 from genetic_algorithm.mutation import NonuniformMutation
 from genetic_algorithm.parent_selection import TournamentSelection
-from genetic_algorithm.survivor_selection import SelectBest
+from genetic_algorithm.survivor_selection import NoSelection
 
 
 class GeneticAlgorithmImpl(GeneticAlgorithm):
@@ -21,13 +21,13 @@ class GeneticAlgorithmImpl(GeneticAlgorithm):
         return TournamentSelection().select_parents(self.population, fitness, worst_from_previous_generations)
 
     def crossover(self, parents: Sequence[Genome]) -> Sequence[Genome]:
-        return OnePointCrossover().crossover(parents)
+        return TwoPointsCrossover().crossover(parents)
 
     def mutate(self, crossovers: Sequence[Genome]) -> Sequence[Genome]:
         return NonuniformMutation().mutate(crossovers)
 
     def select_survivor(self, mutates: Sequence[Genome]) -> Sequence[Genome]:
-        return SelectBest().select_survivor(mutates)
+        return NoSelection().select_survivor(mutates)
 
 
 labeled_sequence = LabeledSequence()
@@ -35,14 +35,16 @@ labeled_sequence.append(FloatGene(label='A', minimum=0.1, maximum=50.0))
 labeled_sequence.append(IntegerGene(label='B', minimum=0, maximum=50))
 labeled_sequence.append(FloatGene(label='C', minimum=0.1, maximum=50.0))
 labeled_sequence.append(IntegerGene(label='D', minimum=0, maximum=50))
+labeled_sequence.append(IntegerGene(label='E', minimum=1, maximum=50))
+labeled_sequence.append(IntegerGene(label='F', minimum=2, maximum=50))
 
 
 def rank(params: LabeledSequence[Gene]):
     return abs(params.get_by_label('A').value + params.get_by_label('B').value + params.get_by_label(
-        'C').value + params.get_by_label('D').value)
+        'C').value + params.get_by_label('D').value + params.get_by_label('E').value + params.get_by_label('F').value)
 
 
 genome = Genome(genes=labeled_sequence, rank_funk=rank)
-algorithm_impl = GeneticAlgorithmImpl(genome=genome, population_size=5, generation_count=1000,
+algorithm_impl = GeneticAlgorithmImpl(genome=genome, population_size=20, generation_count=1000,
                                       best_last_generations_size=5)
 algorithm_impl.calculate()
